acab73259ecd11bfc51c7ee941b82840ada868e8
akorot <akorot@umich.edu>
2020-12-30 11:31:49 -0500

    Fixing Factorial_tail_solution.cpp comment formatting

--- Solutions/Factorial_tail_solution.cpp
+++ Solutions/Factorial_tail_solution.cpp
@@ -1,12 +1,20 @@
 /**
- * Requires: n, k > 0                                                                                                    * Modifies: Nothing                                                                                                     * Effects:  Returns n! using tail recursion                                                                             */                                                                                                                     int factorialHelper(int n, int k) {
+ * Requires: n, k > 0 
+ * Modifies: Nothing     
+ * Effects:  Returns n! using tail recursion   
+ */       
+int factorialHelper(int n, int k) {
     // Base case
     if (n == 1) return k;
     // Tail recursive call
     return factorialHelper(n - 1, k * n);
 }
-                                                                                                                        /**
- * Requires: n > 0                                                                                                       * Modifies: Nothing                                                                                                     * Effects:  Returns n! using the factorialHelper function                                                               */
+
+/**
+ * Requires: n > 0 
+ * Modifies: Nothing           
+ * Effects:  Returns n! using the factorialHelper function        
+ */
 int factorial(n) {
     return factorialHelper(n, 1);
 }
